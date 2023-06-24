1efc25cae70c2311b6b4814e3aafc2e07bb0bfc8
Taylor C. Richberger <taywee@gmx.com>
2023-06-24 17:05:12 -0600

    get plugin fully working

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,87 @@
+use std::borrow::Cow;
+
+use bexpand::Expression;
 use nu_plugin::{EvaluatedCall, LabeledError, Plugin};
 use nu_protocol::{Category, PluginExample, PluginSignature, Type, Value};
 
 pub struct Bexpand;
 
 fn bexpand(call: &EvaluatedCall, input: &Value) -> Result<Value, LabeledError> {
-    let input = match input {
-        Value::String { val, .. } => val.as_str(),
-        _ => {
+    let output: Result<Vec<Cow<str>>, _> = match input {
+        Value::String { val, span, .. } => {
+            let val = val.as_str();
+            let expression: Expression = match val.try_into() {
+                Ok(e) => e,
+                Err(s) => {
+                    return Err(LabeledError {
+                        label: "Brace expression failed to parse".into(),
+                        msg: s,
+                        span: Some(*span),
+                    })
+                }
+            };
+
+            expression.into_iter().collect()
+        }
+        Value::List { vals, .. } => {
+            let exprs: Result<Vec<_>, _> = vals
+                .into_iter()
+                .map(|val| match val {
+                    Value::String { val, span, .. } => {
+                        let val = val.as_str();
+                        let expression: Expression = match val.try_into() {
+                            Ok(e) => e,
+                            Err(s) => {
+                                return Err(LabeledError {
+                                    label: "Brace expression failed to parse".into(),
+                                    msg: s,
+                                    span: Some(*span),
+                                })
+                            }
+                        };
+
+                        Ok(expression.into_iter())
+                    }
+                    v => {
+                        return Err(LabeledError {
+                            label: "Input must be string".into(),
+                            msg: format!("Input type was {}", input.get_type()),
+                            span: v.span().ok(),
+                        })
+                    }
+                })
+                .collect();
+            exprs?.into_iter().flatten().collect()
+        }
+
+        v => {
             return Err(LabeledError {
                 label: "Input must be string".into(),
                 msg: format!("Input type was {}", input.get_type()),
-                span: dbg!(input.span().ok()),
+                span: v.span().ok(),
             })
         }
     };
 
-    todo!("parse and expand brace expression");
+    let output = match output {
+        Ok(o) => o
+            .into_iter()
+            .map(|s| Value::String {
+                val: s.into_owned(),
+                span: call.head,
+            })
+            .collect(),
+        Err(e) => {
+            return Err(LabeledError {
+                label: "Expression failed to generate".into(),
+                msg: e.to_string(),
+                span: input.span().ok(),
+            })
+        }
+    };
 
     Ok(Value::List {
-        vals: vec![Value::String {
-            val: input.into(),
-            span: call.head,
-        }],
+        vals: output,
         span: call.head,
     })
 }
@@ -29,7 +89,13 @@ fn bexpand(call: &EvaluatedCall, input: &Value) -> Result<Value, LabeledError> {
 impl Plugin for Bexpand {
     fn signature(&self) -> Vec<PluginSignature> {
         vec![PluginSignature::build("str bexpand")
-            .input_output_types(vec![(Type::String, Type::List(Box::new(Type::String)))])
+            .input_output_types(vec![
+                (Type::String, Type::List(Box::new(Type::String))),
+                (
+                    Type::List(Box::new(Type::String)),
+                    Type::List(Box::new(Type::String)),
+                ),
+            ])
             .usage("Bash-style brace expansion")
             .plugin_examples(vec![PluginExample {
                 example: "'~/config/nushell/{env,config,plugin}.nu | str bexpand".into(),
