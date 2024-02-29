0e31cf8464961229e77faba9347bd14fcfa81ae0
Taylor C. Richberger <taywee@gmx.com>
2024-02-28 20:26:28 -0700

    give value output for example

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::borrow::Cow;
 
 use bexpand::Expression;
 use nu_plugin::{EvaluatedCall, LabeledError, Plugin};
-use nu_protocol::{Category, PluginExample, PluginSignature, Type, Value};
+use nu_protocol::{Category, PluginExample, PluginSignature, Span, Type, Value};
 
 pub struct Bexpand;
 
@@ -93,9 +93,25 @@ impl Plugin for Bexpand {
             ])
             .usage("Bash-style brace expansion")
             .plugin_examples(vec![PluginExample {
-                example: "'~/config/nushell/{env,config,plugin}.nu' | str bexpand".into(),
+                example: "'~/.config/nushell/{env,config,plugin}.nu' | str bexpand".into(),
                 description: "Get a list of standard nushell config items".into(),
-                result: None,
+                result: Some(Value::List {
+                    vals: vec![
+                        Value::String {
+                            val: "~/.config/nushell/env.nu".into(),
+                            internal_span: Span::new(0, 0),
+                        },
+                        Value::String {
+                            val: "~/.config/nushell/config.nu".into(),
+                            internal_span: Span::new(0, 0),
+                        },
+                        Value::String {
+                            val: "~/.config/nushell/plugin.nu".into(),
+                            internal_span: Span::new(0, 0),
+                        },
+                    ],
+                    internal_span: Span::new(0, 0),
+                }),
             }])
             .category(Category::Strings)]
     }
