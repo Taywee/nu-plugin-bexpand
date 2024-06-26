2821706e617d9c69ce93c10c222e1704c8050fe5
Taylor C. Richberger <taylor@axfive.net>
2024-06-26 16:39:43 -0600

    update nushell to version 0.95

--- src/lib.rs
+++ src/lib.rs
@@ -115,6 +115,10 @@ impl Plugin for BexpandPlugin {
     fn commands(&self) -> Vec<Box<dyn PluginCommand<Plugin = Self>>> {
         vec![Box::new(Bexpand)]
     }
+
+    fn version(&self) -> String {
+        env!("CARGO_PKG_VERSION").into()
+    }
 }
 
 pub struct Bexpand;
